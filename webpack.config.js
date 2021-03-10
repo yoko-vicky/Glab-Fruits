2f304adf23d009f11c6cbb14c5c34dd665b7b3f4
yocosaka <sakayoco55@gmail.com>
2021-03-10 11:50:16 +0900

    Add ImageMinimizerPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 // eslint-disable-next-line import/no-unresolved
 const webpack = require('webpack');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -20,7 +21,20 @@ module.exports = {
       },
       {
         test: /\.(gif|png|jpe?g|svg|xml|mp3|wav)$/i,
-        use: 'file-loader',
+        use: [
+          {
+            loader: 'file-loader',
+          },
+          {
+            loader: ImageMinimizerPlugin.loader,
+            options: {
+              severityError: 'warning',
+              minimizerOptions: {
+                plugins: ['gifsicle'],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.js$/,
@@ -39,6 +53,24 @@ module.exports = {
       'typeof CANVAS_RENDERER': JSON.stringify(true),
       'typeof WEBGL_RENDERER': JSON.stringify(true),
     }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }],
+          [
+            'svgo',
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
   ],
-
 };
